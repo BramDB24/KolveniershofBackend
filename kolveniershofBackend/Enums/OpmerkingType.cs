7bf0f9e26451d9072de880b8170e200a2bb64fa0
kato duwee <kato.duwee@student.hogent.be>
2019-10-07 12:30:39 +0200

    added enum classes

---
+++ kolveniershofBackend/Enums/OpmerkingType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kolveniershofBackend.Enums
+{
+    public enum OpmerkingType
+    {
+        Vervoer, Cliënten, AteliersEnWeekschema, Varia, Logistiek, Begeleiding,
+        Stagiairs, Vrijwilligers, UurRegistratie
+    }
+}
+
