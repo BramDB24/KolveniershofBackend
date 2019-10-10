4894bc35c9917f9132366b85c60f886955568f2c
Bram De Bleecker <bram.debleecker@student.hogent.be>
2019-10-10 10:14:24 +0200

    quick fix dubbel ateliers dbset

--- kolveniershofBackend/Data/ApplicationDbContext.cs
+++ kolveniershofBackend/Data/ApplicationDbContext.cs
@@ -13,7 +13,6 @@ namespace kolveniershofBackend.Data
         public DbSet<DagPlanningTemplate> DagPlanningen { get; set; }
         public DbSet<Atelier> Ateliers { get; set; }
         public DbSet<Gebruiker> Gebruikers { get; set; }
-        public DbSet<Atelier> Ateliers { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
