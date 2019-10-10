072a54c707ba1629664d0202148c43855473e904
Lucas Vermeulen <lucas.vermeulen@student.hogent.be>
2019-10-10 10:18:48 +0200

    remove onnodige commentaar

--- kolveniershofBackend/Startup.cs
+++ kolveniershofBackend/Startup.cs
@@ -30,82 +30,7 @@ namespace kolveniershofBackend
             Configuration = configuration;
         }
 
-        //KolveniershofContext
-
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        /*public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            services.AddDbContext<ApplicationDbContext>(options => {
-                options.UseSqlServer(Configuration.GetConnectionString("KolveniershofContext"));
-            });
-            
-            services.AddScoped<IGebruikerRepository, GebruikerRepository>();
-            services.AddScoped<IDagPlanningTemplateRepository, DagPlanningTemplateRepository>();
-            services.AddScoped<ApplicationDataInitialiser>();
-
-            services.AddAuthentication(x => {
-                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(x => {
-                x.RequireHttpsMetadata = false;
-                x.SaveToken = true;
-                x.TokenValidationParameters = new TokenValidationParameters {
-                ValidateIssuerSigningKey = true,
-                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Tokens:Key"])),
-                ValidateIssuer = false,
-                ValidateAudience = false,
-                RequireExpirationTime = true
-                };
-            });
-
-            //services.AddIdentity<IdentityUser, IdentityRole>(cfg => cfg.User.RequireUniqueEmail = true)
-            //    .AddEntityFrameworkStores<ApplicationDbContext>();
-
-            services.AddIdentity<Gebruiker, IdentityRole>(cfg => cfg.User.RequireUniqueEmail = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-
-            services.AddOpenApiDocument(c =>
-            {
-                c.DocumentName = "apidocs";
-                c.Title = "Kolveniershof API";
-                c.Version = "v1";
-                c.Description = "The Kolveniershof API documentation description.";
-                c.DocumentProcessors.Add(
-                    new SecurityDefinitionAppender("JWT Token", new SwaggerSecurityScheme{
-                        Type = SwaggerSecuritySchemeType.ApiKey,
-                        Name = "Authorization",
-                        In = SwaggerSecurityApiKeyLocation.Header,
-                        Description = "Copy 'Bearer' + valid JWT token into field"
-                    }));
-                c.OperationProcessors.Add(new OperationSecurityScopeProcessor("JWT Token"));
-            });
-         //for OpenAPI 3.0 else AddSwaggerDocument();$
-            services.AddCors(options => options.AddPolicy("AllowAllOrigins", builder => builder.AllowAnyOrigin()));
-
-            /*  services.Configure<IdentityOptions>(options =>
-            {
-                // Password settings.
-                options.Password.RequiredLength = 12;
-                options.Password.RequiredUniqueChars = 1;
-                options.Password.RequireDigit = false;
-                options.Password.RequireUppercase = false;
-                options.Password.RequireNonAlphanumeric = false;
-
-                // Lockout settings.
-                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-                options.Lockout.MaxFailedAccessAttempts = 5;
-                options.Lockout.AllowedForNewUsers = true;
-
-                // User settings.
-                options.User.AllowedUserNameCharacters =
-                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
-                options.User.RequireUniqueEmail = true;
-            });
-
-
-        }*/
 
         public void ConfigureServices(IServiceCollection services)
         {
@@ -201,7 +126,7 @@ namespace kolveniershofBackend
             app.UseMvc();
             app.UseSwaggerUi3();
             app.UseSwagger();
-            dataInitialiser.InitializeData().Wait();
+            //dataInitialiser.InitializeData().Wait();
         }
     }
 }
