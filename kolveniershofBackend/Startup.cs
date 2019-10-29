7587679ba1346d5c44ade7a5b7d292a946cd34da
Lucas Vermeulen <lucas.vermeulen@student.hogent.be>
2019-10-29 16:27:10 +0100

    test remove developer if

--- kolveniershofBackend/Startup.cs
+++ kolveniershofBackend/Startup.cs
@@ -127,15 +127,10 @@ namespace kolveniershofBackend
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ApplicationDataInitialiser dataInitialiser)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
+            
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-            }
+           
             app.UseAuthentication();
             app.UseHttpsRedirection();
             app.UseMvc();
