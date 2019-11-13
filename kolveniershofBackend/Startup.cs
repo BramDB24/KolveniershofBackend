4fc4a61eb2db732e347a351a7ed8726a6319edd0
Lucas Vermeulen <lucas.vermeulen@student.hogent.be>
2019-11-13 12:37:24 +0100

    cors fix

--- kolveniershofBackend/Startup.cs
+++ kolveniershofBackend/Startup.cs
@@ -138,6 +138,8 @@ namespace kolveniershofBackend
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseCors("AllowAllOrigins");
             app.UseAuthentication();
             app.UseHttpsRedirection();
             app.UseMvc();
